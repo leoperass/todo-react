aaa7d26e33ea7a9d5c1d4084e6fc2352afd0a350
Leonardo Perassoli <leonardoperass@gmail.com>
2025-10-16 09:14:16 -0300

    feat: add components Text e Icon
    - Criados componentes Text e Icon
    - Configurado SVGR para transformar SVGs em componente React
    - SVGs transformados em componente React

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+import Icon from "./components/icon";
+
 export default function App() {
   return (
-    <div className="text-">
-      Olá mundo!
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-1">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Olá mundo
+        </Text>
+        <Text className="text-green-base">
+          Olá mundo
+        </Text>
+        <Text variant="body-md-bold">
+          Olá mundo
+        </Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-pink-base"/>
+        <Icon svg={CheckIcon}/>
+        <Icon svg={PencilIcon}/>
+        <Icon svg={PlusIcon}/>
+        <Icon svg={SpinnerIcon} animate/>
+        <Icon svg={XIcon}/>
+      </div>
     </div>
   )
 }
