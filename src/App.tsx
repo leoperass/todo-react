3efc3ae59b75624ae29df97ab9a57f3110b100df
Leonardo Perassoli <leonardoperass@gmail.com>
2025-10-21 08:36:41 -0300

    feat: add componente ButtonIcon com suporte a variantes e Ícones
    - Componente ButtonIcon com variants
    - Suporte a Ícones e tamanhos
    - Integração com CVA para estilos e tipagem

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import XIcon from "./assets/icons/x.svg?react";
 import Icon from "./components/icon";
 import Badge from "./components/badge";
 import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
 
 export default function App() {
   return (
@@ -40,6 +41,12 @@ export default function App() {
 
       <div>
         <Button Icon={PlusIcon}>Nova Tarefa</Button>
+      </div>
+
+      <div className="flex gap-1">
+        <ButtonIcon icon={TrashIcon}/>
+        <ButtonIcon icon={TrashIcon} variant="secondary"/>
+        <ButtonIcon icon={TrashIcon} variant="tertiary"/>
       </div>
     </div>
   )
