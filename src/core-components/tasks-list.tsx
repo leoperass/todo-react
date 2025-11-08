3aec007707f34c281a6f3568b34cc40d2bce1add
Leonardo Perassoli <leonardoperass@gmail.com>
2025-11-08 17:51:09 -0300

    feat: add criação de nova tarefa no TasksList
    - Integração do hook useTask para preparar e adicionar novas tarefas
    - Integração do hook useTasks para leitura do array de tarefas
    - Botão "Nova tarefa" agora adiciona uma tarefa com estado 'Creating'

--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -2,17 +2,23 @@ import Button from "../components/button";
 import PlusIcon from "../assets/icons/plus.svg?react";
 import TaskItem from "./task-item";
 import useTasks from "../hooks/use-tasks";
+import { useTask } from "../hooks/use-task";
 
 
 export default function TasksList() {
     const {tasks} = useTasks();
+    const { prepareTask } = useTask();
 
     console.log(tasks);
+
+    function handleNewTask() {
+        prepareTask();
+    }
 
     return (
         <>
             <section>
-                <Button Icon={PlusIcon} className="w-full">Nova tarefa</Button>
+                <Button Icon={PlusIcon} className="w-full" onClick={handleNewTask}>Nova tarefa</Button>
             </section>
 
             <section className="space-y-2">
