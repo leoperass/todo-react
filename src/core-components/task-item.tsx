4905442123e231e318f7f22ccdec01034fc311b0
Leonardo Perassoli <leonardoperass@gmail.com>
2025-11-11 19:20:48 -0300

    feat(use-task / task-item): adicionar updateTask para edição de tarefas
    useTask: -adicionada a função 'updateTask' para atualizar o título de tarefas específicas no localStorage.
    - mantém o estado da tarefa como 'Created' ao salvar.
    
    TaskItem: - Integrado 'updateTask' para salvar alterações de título da tarefa.
    - InputText agora é controlado pelo estado 'taskTitle'.
    - handleSaveTask atualiza a tarefa no localStorage e encerra a edição.

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -10,6 +10,7 @@ import XIcon from "../assets/icons/x.svg?react";
 import CheckIcon from "../assets/icons/check.svg?react";
 import { cx } from "class-variance-authority";
 import { TaskState, type Task } from "../models/task";
+import { useTask } from "../hooks/use-task";
 
 interface TaskItemProps {
     task: Task;
@@ -20,7 +21,8 @@ export default function TaskItem({ task }: TaskItemProps) {
     const [IsEditing, setIsEditing] = React.useState(
         task?.state === TaskState.Creating
     );
-    const [taskTitle, setTaskTitle] = React.useState("");
+    const [taskTitle, setTaskTitle] = React.useState(task.title || "");
+    const { updateTask } = useTask();
 
     function handleEditTask() {
         setIsEditing(true);
@@ -36,8 +38,9 @@ export default function TaskItem({ task }: TaskItemProps) {
 
     function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        console.log({id: task.id, taskTitle});
-        // chamada para função de atualizar;
+
+        updateTask(task.id, {title: taskTitle});
+
         setIsEditing(false);
     }
 
@@ -64,6 +67,7 @@ export default function TaskItem({ task }: TaskItemProps) {
                 :
                 <form onSubmit={handleSaveTask} className="flex items-center gap-4">
                     <InputText
+                     value={taskTitle}
                      className="flex-1" 
                      onChange={handleChangeTaskTitle} 
                      required 
